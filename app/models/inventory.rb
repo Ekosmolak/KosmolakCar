c7e163e7112b287e55c0bb6513b6ede961a521df
Ekosmolak <ekosmolak@gmail.com>
2024-10-22 01:55:41 -0500

    Added associations

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,4 @@
 class Inventory < ApplicationRecord
   validates :inventoryId, :vehicleId, :manufactorId, presence: true
+  belongs_to :Vehicle, :Manufactor
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,4 @@
 class Vehicle < ApplicationRecord
   validates :vehicleId, :manufactorId, :model, :colour, :price, :odometer, :vehicleType, presence: true
+  belongs_to :Manufactor
 end
